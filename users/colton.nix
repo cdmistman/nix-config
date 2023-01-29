0603a4562a04542b34c64a45991eb06981904abd
Colton Donnelly <colton@donn.io>
2023-01-29 11:02:45 -0800

    more starship configs

--- users/colton.nix
+++ users/colton.nix
@@ -88,18 +88,22 @@ in
 			starship = enabled // withZsh // {
 				settings = {
 					add_newline = false;
-					format = "$username"
-						+ "$hostname"
+					format = "(\\[$username$hostname\\] )"
 						+ "$directory"
 						+ "$git_branch"
+						+ "$git_metrics"
 						+ "$git_state"
 						+ "$git_status"
 						+ "$cmd_duration"
 						+ "$line_break"
+						+ "$sudo"
+						+ "$jobs"
+						+ "$shell"
 						+ "$character";
 
 					character = {
-						success_symbol = "[❯](purple)";
+						format = "[$symbol ]($style)";
+						success_symbol = "[](purple)";
 						error_symbol = "[❯](red)";
 						vimcmd_symbol = "[❮](green)";
 					};
@@ -109,16 +113,24 @@ in
 					};
 
 					directory = {
-						style = "blue";
+						style = "bright-blue";
 					};
 
 					git_branch = {
-						format = "[$branch]($style)";
+						format = "[$branch(:$remote_branch)]($style)";
+						style = "208";
+					};
+					git_metrics = {
+						format = "([|](dimmed white)[+$added]($added_style)[-$deleted]($deleted_style))";
+						disabled = false;
+					};
+					git_state = {
+						format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
 						style = "bright-black";
 					};
 					git_status = {
-						format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-						style = "cyan";
+						format = "([|](dimmed white)[$ahead$behind$conflicted$untracked$modified$staged$renamed$deleted$stashed]($style)) ";
+						style = "219";
 						conflicted = "";
 						untracked = "";
 						modified = "";
@@ -127,9 +139,36 @@ in
 						deleted = "";
 						stashed = "≡";
 					};
-					git_state = {
-						format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
+
+					hostname = {
+						format = "[$hostname]($style)( $ssh_symbol)";
+						ssh_only = false;
+						ssh_symbol = "";
 						style = "bright-black";
+					};
+
+					jobs = {
+						format = "([$number]($style) )";
+						number_threshold = 1;
+						style = "bg:168";
+						symbol = "華";
+					};
+
+					shell = {
+						disabled = false;
+						format = "[$indicator]($style)";
+					};
+					sudo = {
+						disabled = false;
+						format = "[$symbol]($style)";
+						symbol = "✦ ";
+						style = "224";
+					};
+
+					username = {
+						format = "([$user]($style)@)";
+						show_always = true;
+						style_user = "bright-purple";
 					};
 				};
 			};
@@ -154,15 +193,6 @@ in
 
 				oh-my-zsh = enabled // {
 					plugins = [
-						# "deno"
-						# "direnv"
-						# "fnm"
-						# "gh"
-						# "git"
-						# "golang"
-						# "man"
-						# "ripgrep"
-						# "rust"
 						"vi-mode"
 					];
 				};
