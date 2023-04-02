4260fe45625f8d1fdfd7c5cfcc79c9ccb3e4075d
Colton Donnelly <colton@donn.io>
2023-04-01 18:43:37 -0700

    [rust] add rust shell

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+		devenv = {
+			url = "github:cachix/devenv";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		fenix = {
+			url = "github:nix-community/fenix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		flake-parts.url = "github:hercules-ci/flake-parts";
+	};
+
+	outputs = inputs @ {
+		flake-parts,
+		...
+	}:
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			imports = [
+				inputs.devenv.flakeModule
+			];
+
+			systems = [
+				"aarch64-darwin"
+			];
+
+			perSystem = { lib, pkgs, system, ... }: let
+				darwinFrameworks = lib.attrValues (
+					lib.filterAttrs
+						(k: _: k != "QuickTime" && k != "QTKit")
+						pkgs.darwin.apple_sdk.frameworks
+				);
+			in {
+				devenv.shells.rust = {
+					packages = darwinFrameworks ++ [
+						pkgs.cargo-deny
+						pkgs.clippy
+						pkgs.cmakeMinimal
+					];
+
+					env.CPATH = pkgs.lib.makeSearchPath "include" [
+						pkgs.darwin.libobjc
+					];
+
+					env.LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+						pkgs.darwin.libobjc
+						pkgs.iconv
+						pkgs.llvmPackages.libcxxabi
+					];
+
+					languages.rust.enable = true;
+				};
+			};
+		};
+}
